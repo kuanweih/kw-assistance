148632a8adbbf42e697aced6fd0d805520e9775b
kuanweih <kuanweih@andrew.cmu.edu>
2019-05-16 23:02:12 -0400

    draft setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,21 @@
-test
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kwbot",
+    version="0.0.1",
+    author="Kuan-Wei Huang",
+    author_email="kuanweih@andrew.cmu.edu",
+    description="Some useful functions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kuanweih/kwbot",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
